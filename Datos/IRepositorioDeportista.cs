45beb45a34f67f3feba3c4b2187ba142125851cd
William Alexis Ortiz Perea <waortiz@hotmail.com>
2019-03-19 08:40:30 -0500

    Ajustes

---
+++ Datos/IRepositorioDeportista.cs
@@ -0,0 +1,14 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public interface IRepositorioDeportista
+    {
+        int IngresarDeportista(Deportista deportista);
+    }
+}
