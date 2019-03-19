57797b423d4b52d7d2c211425a9bda4b8ff9c0f9
William Alexis Ortiz Perea <waortiz@hotmail.com>
2019-03-19 08:37:31 -0500

    Ajustes

---
+++ Entidades/TipoDocumentoEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public enum TipoDocumentoEnum
+    {
+        Cedula = 1,
+        TarjetaIdentidad = 2,
+        CedulaExtranjeria = 3,
+        PermisoEspecial = 4
+    }
+}
