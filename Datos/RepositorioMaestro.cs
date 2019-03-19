01845794e7d654123043591894e7fcbaf51f6a85
William Alexis Ortiz Perea <waortiz@hotmail.com>
2019-03-19 08:36:38 -0500

    Ajustes

---
+++ Datos/RepositorioMaestro.cs
@@ -0,0 +1,17 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class RepositorioDeportista : IRepositorioDeportista
+    {
+        public int IngresarDeportista(Deportista deportista)
+        {
+            return default(int);
+        }
+    }
+}
