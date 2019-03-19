679bfefa10436f45d73717d7ea133c6f2309d12f
William Alexis Ortiz Perea <waortiz@hotmail.com>
2019-03-19 08:38:44 -0500

    Ajustes

---
+++ Negocio/ControlMaestro.cs
@@ -0,0 +1,26 @@
+﻿using Datos;
+using Entidades;
+using System.Collections.Generic;
+
+namespace Negocio
+{
+    public class ControlMaestro : IControlMaestro
+    {
+        private readonly IRepositorioMaestro repositorio;
+
+        public ControlMaestro()
+        {
+            repositorio = new RepositorioMaestro();
+        }
+
+        public ControlMaestro(IRepositorioMaestro repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        public List<TipoDocumento> ObtenerTiposDocumento()
+        {
+            return repositorio.ObtenerTiposDocumento();
+        }
+}
+}
---
+++ Negocio/IControlDeportista.cs
@@ -0,0 +1,15 @@
+﻿using Datos;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public interface IControlDeportista
+    {
+        int IngresarDeportista(Deportista deportista);
+    }
+}
